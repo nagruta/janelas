2884d7b083fdc285041d26fde99b66fe27ea5475
Christopher Augustus <repos@c4augustus.com>
2020-05-15 14:59:44 +0100

    create subscript sub-establish-file-download.ps1

---
+++ sub-establish-file-download.ps1
@@ -0,0 +1,11 @@
+﻿# establish file already exists or else download it
+$targetpath = $args[0]
+$targetfile = $args[1]
+$urlbase = $args[2]
+$targetspec = "$targetpath/$targetfile"
+$sourceurl = "$urlbase$targetfile"
+./sub-establish-path $args[0]
+if (-not (Test-Path "${targetspec}")) {
+  "downloading $targetspec from $sourceurl"
+  Invoke-WebRequest -Uri $sourceurl -OutFile $targetspec -UseBasicParsing
+}
