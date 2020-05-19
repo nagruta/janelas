5a2ba06546d7b8bcd9bc8850bc055cfd3e20c755
Christopher Augustus <repos@c4augustus.com>
2020-05-19 08:12:00 +0100

    change establish-dist-all.ps1 to sub-establish-dist-all.ps1

--- establish-dist-all.ps1
+++
@@ -1,3 +0,0 @@
-﻿# establish required software distributions
-./sub-establish-dist-download "vagrant_2.2.9_x86_64.msi" "https://releases.hashicorp.com/vagrant/2.2.9/"
-./sub-establish-dist-download "VirtualBox-6.1.6-137129-Win.exe" "https://download.virtualbox.org/virtualbox/6.1.6/"
