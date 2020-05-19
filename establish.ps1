a0f35a8e8320e34b2b6ae4c20f61ec2508b48473
Christopher Augustus <repos@c4augustus.com>
2020-05-19 08:13:30 +0100

    create establish.ps1: the main script that installs and sets up everything, idempotently

---
+++ establish.ps1
@@ -0,0 +1,9 @@
+﻿# establish installation and setup, idempotently
+$scriptdir = Split-Path -Path $MyInvocation.MyCommand.Definition
+cd $scriptdir
+./sub-establish-dist-all
+# TODO: @@@ chocolately is too complicated to deal with for now
+#./sub-install-chocolatey
+./sub-install-virtualbox
+./sub-install-vagrant
+./sub-setup-vagrant
