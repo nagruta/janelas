d6ddc1202517bd7cf6d552450604beda0c2c8ff3
Christopher Augustus <repos@c4augustus.com>
2020-05-22 17:26:23 +0100

    move and rename begin-set-policy-by-hand.ps1 to a/by-hand-set-executionpolicy.ps1

---
+++ a/by-hand-set-executionpolicy.ps1
@@ -0,0 +1,4 @@
+﻿# THIS MUST TYPED INTO POWERSHELL BY HAND, otherwise no other powershell scripts will execute
+# based on https://www.darkoperator.com/blog/2013/3/5/powershell-basics-execution-policy-part-1.html
+set-executionpolicy bypass -scope currentuser
+get-executionpolicy -list
--- begin-set-policy-by-hand.ps1
+++
@@ -1,3 +0,0 @@
-﻿# based on https://www.darkoperator.com/blog/2013/3/5/powershell-basics-execution-policy-part-1.html
-set-executionpolicy bypass -scope currentuser
-get-executionpolicy -list
