3cad7070c62de2996ffb8b22ec871acb0efa9eda
Christopher Augustus <repos@c4augustus.com>
2020-09-12 23:10:17 +0100

    fix Vagrantfile provision installation of python-pip and pywinrm: sudo apt update was needed; also try other boxes to less avail: ubuntu/bionic64 ans ansible/tower (not Ubuntu)

--- a/ps/sub/sub-install-vagrant.ps1
+++ a/ps/sub/sub-install-vagrant.ps1
@@ -6,6 +6,8 @@ if (-not (Test-Path $installdir)) {
 #$defaultbox = alisio/awx --box-version 1.0.0
 #$defaultbox = "darkwizard242/devopsubuntu1804"
 $defaultbox = "darkwizard242/ansibleubuntu1804"
+#$defaultbox = "ubuntu/bionic64"
+#$defaultbox = "ansible/tower"
 if (-not $(vagrant box list).Contains($defaultbox)) {
   vagrant box add $defaultbox
 }
