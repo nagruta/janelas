f1b0fc3e79591720622b65b524cfe8dec97b06b9
Christopher Augustus <repos@c4augustus.com>
2020-09-23 21:30:27 +0100

    change default vagrant box to "rdf/ubuntu2004-devops", and sync folder /Users to /mnt/home

--- a/ps/sub/sub-install-vagrant.ps1
+++ a/ps/sub/sub-install-vagrant.ps1
@@ -3,11 +3,13 @@ if (-not (Test-Path $installdir)) {
   ./dist/msi/vagrant_2.2.9_x86_64.msi
 }
 #$defaultbox = hashicorp/bionic64
-#$defaultbox = alisio/awx --box-version 1.0.0
-#$defaultbox = "darkwizard242/devopsubuntu1804"
-$defaultbox = "darkwizard242/ansibleubuntu1804"
 #$defaultbox = "ubuntu/bionic64"
 #$defaultbox = "ansible/tower"
+#$defaultbox = "alisio/awx --box-version 1.0.0"
+#$defaultbox = "darkwizard242/devopsubuntu1804"
+#$defaultbox = "darkwizard242/ansibleubuntu1804"
+#$defaultbox = "generic/ubuntu2004"
+$defaultbox = "rdf/ubuntu2004-devops"
 if (-not $(vagrant box list).Contains($defaultbox)) {
   vagrant box add $defaultbox
 }
