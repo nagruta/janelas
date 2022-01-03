09349b0dbfb1062677c42b1b7fef7c34385b6f74
Christopher Augustus <repos@c4augustus.com>
2022-01-03 15:22:17 +0000

    fix a/ps/sub/sub-install-vagrant.ps1 to deal with the limitations of GUI installation: check for VirtualBox installed first, prevent post-installation restart, add to PATH prior to restart, and set VAGRANT_HOME

--- a/ps/sub/sub-install-vagrant.ps1
+++ a/ps/sub/sub-install-vagrant.ps1
@@ -1,7 +1,21 @@
-﻿$installdir = "C:\opt\Vagrant"
+﻿$installdir = "C:\opt\VirtualBox"
 if (-not (Test-Path $installdir)) {
-  ./dist/msi/vagrant_2.2.9_x86_64.msi
+  "VirtualBox is NOT yet installed. Run script again after it is."
+  exit
 }
+$installdir = "C:\opt\Vagrant"
+if (-not (Test-Path $installdir)) {
+  ../../dist/msi/vagrant_2.2.9_x86_64.msi /norestart /qn INSTALLDIR=$installdir
+  # until PATH is updated after restart
+  $env:Path += ";$installdir\bin"
+}
+# TODO: ### THE INSTALLER ABOVE IS A GUI SO WE MUST WAIT FOR IT TO COMPLETE
+if (-not (Test-Path $installdir)) {
+  "Vagrant is NOT yet installed. Run script again after it is."
+  exit
+}
+$scriptdir = Split-Path -Path $MyInvocation.MyCommand.Definition
+iex "$scriptdir/sub-env-vagrant-home"
 #$defaultbox = hashicorp/bionic64
 #$defaultbox = "ubuntu/bionic64"
 #$defaultbox = "ansible/tower"
@@ -10,6 +24,8 @@ if (-not (Test-Path $installdir)) {
 #$defaultbox = "darkwizard242/ansibleubuntu1804"
 #$defaultbox = "generic/ubuntu2004"
 $defaultbox = "rdf/ubuntu2004-devops"
+#cd $installdir/bin
+#$env:Path += ";$installdir\bin"
 if (-not $(vagrant box list).Contains($defaultbox)) {
   vagrant box add $defaultbox
 }
