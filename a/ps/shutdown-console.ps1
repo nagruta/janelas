363396e49075e9876342c21316ced27504d2d1e7
Christopher Augustus <repos@c4augustus.com>
2022-06-10 16:09:50 +0100

    add powershell script a/ps/shutdown-console.ps1 that calls vagrant destroy

---
+++ a/ps/shutdown-console.ps1
@@ -0,0 +1,7 @@
+﻿# launch vagrant
+param ([string]$vmedir = 'console')
+Push-Location
+$scriptdir = Split-Path -Path $MyInvocation.MyCommand.Definition
+cd "$scriptdir/../../vme/$vmedir"
+vagrant destroy
+Pop-Location
